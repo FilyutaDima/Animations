d0ec98b4020c5a8d8d7d0661ebe5d768d6982241
Dmitry <filyuta.dima@yandex.ru>
2022-02-25 17:59:52 +0300

    Task solved

---
+++ Animations/Animations/DataSource/AnimationType.swift
@@ -0,0 +1,45 @@
+//
+//  AnimationType.swift
+//  Animations
+//
+//  Created by Dmitry on 25.02.22.
+//
+
+import Foundation
+
+enum AnimationType: String, CaseIterable {
+    case shake
+    case pop
+    case morph
+    case squeeze
+    case wobble
+    case swing
+    case flipX
+    case flipY
+    case fall
+    case squeezeLeft
+    case squeezeRight
+    case squeezeDown
+    case squeezeUp
+    case slideLeft
+    case slideRight
+    case slideDown
+    case slideUp
+    case fadeIn
+    case fadeOut
+    case fadeInLeft
+    case fadeInRight
+    case fadeInDown
+    case fadeInUp
+    case zoomIn
+    case zoomOut
+    case flash
+}
+
+enum Curve: String, CaseIterable {
+    case spring
+    case linear
+    case easeIn
+    case easeOut
+    case easeInOut
+}
---
+++ Animations/Animations/DataSource/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Animations
+//
+//  Created by Dmitry on 25.02.22.
+//
+
+import Foundation
+
+class Constants {
+    static let goToSideMenu = "goToSideMenu"
+    static let goToRootViewController = "goToRootViewController"
+}
+
